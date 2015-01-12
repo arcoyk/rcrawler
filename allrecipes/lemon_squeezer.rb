f2ecde5d45b2a6a0c59de0bc5c3b9b821b44a76e
arcoyk <sanhai.sato@gmail.com>
2015-01-13 02:18:30 +0900

    safe

--- allrecipes/lemon_squeezer.rb
+++ allrecipes/lemon_squeezer.rb
@@ -14,12 +14,18 @@ end
 
 db = SQLite3::Database.new "recipe.db"
 ing_lists = db.execute("SELECT ingredients FROM recipes LIMIT 10")
+ing_lists = [["butter &&&& bean &&&& tomato"],
+			["butter &&&& orange &&&& chocolate &&&& sugar"],
+			["sugar &&&& bean"],
+			["sugar &&&& orange &&&& butter"],
+			["olive oil &&&& tomato &&&& pasta"]]
 recipes = []
 ing_lists.each do |ing_list|
 	ings = []
 	ing_list.last.split("&&&&").each do |ing|
 		ing.delete!(',')
 		ing.delete!(';')
+		ing.delete!(':')
 		ing.delete!('*')
 		ing.strip!
 		ing.downcase!
@@ -30,13 +36,13 @@ ing_lists.each do |ing_list|
 	recipes.push ings
 end
 
+p recipes
+
 $list = recipes.flatten
 $list.sort! do |ing1, ing2|
 	$list.count(ing1) <=> $list.count(ing2)
 end
 $list.uniq!.reverse!
-
-puts $list
 
 length = $list.length
 p length
@@ -68,34 +74,44 @@ $list.each do |item|
 	end
 end
 
-# fold map
-puts "folding"
+
+# zero
 length.times do |row|
 	length.times do |col|
-		$link_map[col][row] *= 2
 		if col == row
 			$link_map[col][row] = 0
 		end
 	end
 end
 
+show_map $link_map
+
 # find shortest path
 link_summary = []
+max_val = $link_map.flatten.max
 length.times do |row|
 	length.times do |col|
 		val = $link_map[col][row]
 		if val != 0
-			link_summary.push [col, row, val]
+			link_summary.push [col, row, max_val - val]
 		end
 	end
 end
 
 link_summary.unshift [link_summary.length]
 puts "searching shortest path"
-result = Dijkstra.new(1, 5, link_summary)
+result = Dijkstra.new(2, 4, link_summary)
 path = result.getShortestPath()
-puts $list[1]
-puts $list[5]
 path.each do |index|
 	p $list[index]
 end
+p path
+p result.getCost()
+result = Dijkstra.new(4, 2, link_summary)
+path = result.getShortestPath()
+path.each do |index|
+	p $list[index]
+end
+p path
+p result.getCost()
+
